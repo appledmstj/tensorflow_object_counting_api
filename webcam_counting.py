66191f8b0e198524da26f8271d18d00d3a6e48db
appledmstj <44135431+appledmstj@users.noreply.github.com>
2020-12-09 00:25:58 +0900

    Add files via upload

--- webcam_counting.py
+++ webcam_counting.py
@@ -15,5 +15,7 @@ from api import object_counting_api
 detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28', 'mscoco_label_map.pbtxt')
 
 is_color_recognition_enabled = 0
+roi = 300
+deviation = 10
 
-object_counting_api.object_counting_webcam(detection_graph, category_index, is_color_recognition_enabled)
+object_counting_api.object_counting_webcam(detection_graph, category_index, is_color_recognition_enabled, roi, deviation)
